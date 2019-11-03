f5074e7fa9fcd3286b80e2af0e2f1817e4cc9bc6
LeePeiLin <747127514@qq.com>
2019-11-03 13:36:04 +0800

    提交：主界面

--- app/src/main/java/com/brins/riceweather/ui/BaseActivity.kt
+++ app/src/main/java/com/brins/riceweather/ui/BaseActivity.kt
@@ -5,6 +5,8 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.brins.riceweather.R
+import com.jaeger.library.StatusBarUtil
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -12,11 +14,26 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= 23) {
+/*        if (Build.VERSION.SDK_INT >= 23) {
             val decorView = window.decorView
             decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            window.statusBarColor = Color.TRANSPARENT
+            window.statusBarColor = getColor(R.color.translate)
+        }*/
+    }
+
+    protected open fun getOffsetView(): View? {
+        return null
+    }
+
+    protected fun isStatusBarTranslucent(): Boolean {
+        return true
+    }
+
+    protected fun setStatusBarTranslucent() {
+        if (!isStatusBarTranslucent()) {
+            return
         }
+        StatusBarUtil.setTranslucentForImageView(this, 0, getOffsetView())
     }
 
 //    protected abstract fun getLayoutResId(): Int
--- app/src/main/java/com/brins/riceweather/ui/MainActivity.kt
+++ app/src/main/java/com/brins/riceweather/ui/MainActivity.kt
@@ -2,12 +2,20 @@ package com.brins.riceweather.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.brins.riceweather.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setStatusBarTranslucent()
+
+    }
+
+    override fun getOffsetView(): View? {
+        return titleView
     }
 }
